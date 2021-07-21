4bc919acb02267fb3fc57c34749bd26ed41c05f9
Moon <60549575+mymoonyue@users.noreply.github.com>
2021-07-21 18:57:03 +0800

    Update caiyun_svip.js

--- Scripts/scripts/caiyun_svip.js
+++ Scripts/scripts/caiyun_svip.js
@@ -2,6 +2,11 @@
  * [MITM]
  * hostname = biz.caiyunapp.com
  * 
+ * Quantumult X
+ * [rewrite_local]
+ * ^https?:\/\/biz\.caiyunapp\.com\/(membership_rights|v2\/user) url script-response-body https://raw.githubusercontent.com/mymoonyue/QuantumultX/master/Scripts/scripts/caiyun_svip.js
+ *
+ * Surge
  * [Script]
  * 彩云天气_SVIP = type=http-response,requires-body=1,max-size=0,pattern=https?:\/\/biz\.caiyunapp\.com\/(membership_rights|v2\/user),script-path=https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/script/caiyun/caiyun_svip.js
  * 
